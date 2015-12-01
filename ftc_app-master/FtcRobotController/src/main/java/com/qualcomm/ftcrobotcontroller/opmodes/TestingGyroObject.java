833a5198c33d572a0d9e91852d06f9cafc320a7d
noah <moogie0203@gmail.com>
2015-12-01 18:38:13 -0500

    fixed TestingGyroObject date:12/1/15

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestingGyroObject.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestingGyroObject.java
@@ -18,48 +18,7 @@ public class TestingGyroObject extends LinearOpMode {
         GyroObject degrees = new GyroObject(leftDriveMotorArg, rightDriveMotorArg, gyroArg);
         telemetry.addData("before start", "");
         waitForStart();
-        telemetry.addData("start", "");
         degrees.turnGyro(90);
-        telemetry.addData("turn 90", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep", "");
 
-        degrees.turnGyro(90);
-        telemetry.addData("turn 90 2", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep2", "");
-
-        degrees.turnGyro(90);
-        telemetry.addData("turn 90", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep3", "");
-
-        degrees.turnGyro(90);
-        telemetry.addData("turn 90", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep4", "");
-
-
-
-
-        degrees.turnGyro(-90);
-        telemetry.addData("turn -90 1", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep5", "");
-
-        degrees.turnGyro(-90);
-        telemetry.addData("turn -90 2", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep6", "");
-
-        degrees.turnGyro(-90);
-        telemetry.addData("turn -90 3", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep7", "");
-
-        degrees.turnGyro(-90);
-        telemetry.addData("turn -90 4", "");
-        s.Sleep(1000);
-        telemetry.addData("sleep8", "");
     }
 }
