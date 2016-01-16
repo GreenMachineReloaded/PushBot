e0b132a0505190f3620897c352766e2f7731f654
Andor <pstoney16@gmail.com>
2016-01-16 14:13:38 -0500

    Added sweeper code to the simlpeCadwyn program.

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CadwynSimpleDrive.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CadwynSimpleDrive.java
@@ -10,11 +10,13 @@ public class CadwynSimpleDrive extends OpMode {
 
     DcMotor leftDriveMotor;
     DcMotor rightDriveMotor;
+    DcMotor sweeperMotor;
 
     @Override
     public void init() {
         leftDriveMotor = hardwareMap.dcMotor.get("leftDriveMotor");
         rightDriveMotor = hardwareMap.dcMotor.get("rightDriveMotor");
+        sweeperMotor = hardwareMap.dcMotor.get("sweeperMotor");
 
         leftDriveMotor.setDirection(DcMotor.Direction.FORWARD);
         rightDriveMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -27,5 +29,13 @@ public class CadwynSimpleDrive extends OpMode {
 
         leftDriveMotor.setPower(moveLeft);
         rightDriveMotor.setPower(moveRight);
+
+        if (gamepad1.right_bumper) {
+            sweeperMotor.setPower(1);
+        } else if (gamepad1.right_trigger > 0) {
+            sweeperMotor.setPower(-1);
+        } else {
+            sweeperMotor.setPower(0);
+        }
     }
 }
