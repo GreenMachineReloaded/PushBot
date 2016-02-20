f2b3d02aaf983e2a1b375b7e117353ce175d3b4f
noah <moogie0203@gmail.com>
2016-02-20 14:37:08 -0500

    working testUltrasonicObject code and ultrasonicObject

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/UltrasonicObject.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/UltrasonicObject.java
@@ -6,12 +6,16 @@ import com.qualcomm.robotcore.robocol.Telemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class UltrasonicObject {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //verbals
     AnalogInput sonic;
     DcMotor RightMotor;
     DcMotor LeftMotor;
     double range;
     Sleeper s;
-    Telemetry t;
+    //verbals
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //constructor
     public UltrasonicObject(AnalogInput ArgUltrasonicSensor) {
         sonic = ArgUltrasonicSensor;
         s = new Sleeper();
@@ -23,50 +27,64 @@ public class UltrasonicObject {
         LeftMotor = ArgLeftMotor;
 
     }
+    //constructor
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //returns range
     public double getRangeValue() {
         range = sonic.getValue();
         return range;
     }
+
+
     public double getRangeInches() {
-        range = getRangeValue() / 6;
+        range = sonic.getValue() / 6;
         return range;
     }
+
+
     public double getRangeCentimeters() {
-        range = getRangeInches() / 2.54;
+        range = (sonic.getValue()/6)/ 2.54;
         return range;
     }
+    //returns range
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //go distance
     public void GoDistanceValue(double distance) {
         RightMotor.setPower(1.00);
         LeftMotor.setPower(1.00);
         while(range >= distance) {
             s.Sleep(50);
-            range = getRangeValue();
+            range = sonic.getValue();
         }
         RightMotor.setPower(0);
         LeftMotor.setPower(0);
     }
+
+
     public void GoDistanceInches(double distance) {
         RightMotor.setPower(1.00);
         LeftMotor.setPower(1.00);
         while(range >= distance) {
             s.Sleep(50);
-            range = getRangeInches();
+            range = sonic.getValue()/6;
         }
         RightMotor.setPower(0);
         LeftMotor.setPower(0);
     }
+
+
     public void GoDistanceCentimeters(double distance) {
 
         RightMotor.setPower(.50);
         LeftMotor.setPower(.50);
-        t.addData("range centimeter", getRangeCentimeters());
         s.Sleep(2000);
         while(range >= distance) {
-            t.addData("range centimeter loup", getRangeCentimeters());
             s.Sleep(50);
-            range = getRangeCentimeters();
+            range = (sonic.getValue()/6)/2.54;
         }
         RightMotor.setPower(0);
         LeftMotor.setPower(0);
     }
+    //go distance
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
